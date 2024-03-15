f8a440f9933e3b073734ad2ef702a0f3215e0ba8
GeorgeD615 <g.davlyatshin@gmail.com>
2024-03-15 14:01:27 +0300

    1042 -> implementation

---
+++ Davlyatshin/Davlyatshin/1042.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Davlyatshin
+{
+    public static partial class Solution
+    {
+        public static int[] GardenNoAdj(int n, int[][] paths)
+        {
+            var graph = new List<int>[n];
+            for (int i = 0; i < n; ++i)
+                graph[i] = new List<int>();
+
+            foreach (var path in paths)
+            {
+                graph[path[0] - 1].Add(path[1] - 1);
+                graph[path[1] - 1].Add(path[0] - 1);
+            }
+
+            int[] result = new int[n];
+
+            var used = new bool[4];
+
+            for (int i = 0; i < n; ++i)
+            {
+                foreach (var vertex in graph[i])
+                    if (result[vertex] != 0)
+                        used[result[vertex] - 1] = true;
+                for (int j = 0; j < 4; ++j)
+                    if (!used[j])
+                    {
+                        result[i] = j + 1;
+                        break;
+                    }
+                for (int k = 0; k < 4; ++k)
+                    used[k] = false;
+            }
+
+            return result;
+        }
+    }
+}
--- Davlyatshin/Davlyatshin/Program.cs
+++ Davlyatshin/Davlyatshin/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Davlyatshin;
+
+var result = Solution.GardenNoAdj(4, [[1, 2], [3, 4]]);
+Console.WriteLine(string.Join(" ", result));
