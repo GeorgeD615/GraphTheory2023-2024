4fe67ec0f229f6d2d87ef9150b52cab636ae1281
GeorgeD615 <g.davlyatshin@gmail.com>
2024-03-21 21:02:57 +0300

    implementation -> 1697

---
+++ Davlyatshin/Davlyatshin/1697.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Davlyatshin
+{
+    public static partial class Solution
+    {
+        private static int[] dsuTree;
+        private static void MakeSet(int v)
+        {
+            dsuTree[v] = v;
+        }
+
+        private static int Find(int v)
+        {
+            if (dsuTree[v] == v) return v;
+            return dsuTree[v] = Find(dsuTree[v]);
+        }
+
+        private static void Union(int u, int v)
+        {
+            u = Find(u);
+            v = Find(v);
+            dsuTree[u] = v;
+        }
+
+        public static bool[] DistanceLimitedPathsExist(int n, int[][] edgeList, int[][] queries)
+        {
+            var result = new bool[queries.Length];
+            dsuTree = new int[n];
+
+            for(int i = 0; i < n; ++i)
+                MakeSet(i);
+
+            edgeList = edgeList.OrderBy(edje => edje[2]).ToArray();
+
+            for (int i = 0; i < queries.Length; i++)
+                queries[i] = [queries[i][0], queries[i][1], queries[i][2], i];
+
+            queries = queries.OrderBy(query => query[2]).ToArray();
+
+            int k = 0;
+            for(int i = 0; i < queries.Length; ++i)
+            {
+                int[] query = queries[i];
+                while (k < edgeList.Length && edgeList[k][2] < query[2])
+                {
+                    int u = edgeList[k][0];
+                    int v = edgeList[k][1];
+                    Union(u, v);
+                    ++k;
+                }
+                int p = query[0];
+                int q = query[1];
+                int queryIndex = query[3];
+                if (Find(p) == Find(q))
+                {
+                    result[queryIndex] = true;
+                }
+            }
+            return result;
+        }
+    }
+}
--- Davlyatshin/Davlyatshin/Program.cs
+++ Davlyatshin/Davlyatshin/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Davlyatshin;
-
+﻿using Davlyatshin;
 
 var result = Solution.GardenNoAdj(4, [[1, 2], [3, 4]]);
 Console.WriteLine(string.Join(" ", result));
@@ -20,3 +18,17 @@ Console.WriteLine(result3);
 
 var result4 = Solution.EdgeScore([1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
 Console.WriteLine(result4);
+
+
+var result5 = Solution.DistanceLimitedPathsExist(13, [[9, 1, 53], [3, 2, 66], [12, 5, 99], [9, 7, 26], [1, 4, 78], [11, 1, 62], 
+                                                    [3, 10, 50], [12, 1, 71], [12, 6, 63], [1, 10, 63], [9, 10, 88], [9, 11, 59], 
+                                                    [1, 4, 37], [4, 2, 63], [0, 2, 26], [6, 12, 98], [9, 11, 99], [4, 5, 40], 
+                                                    [2, 8, 25], [4, 2, 35], [8, 10, 9], [11, 9, 25], [10, 11, 11], [7, 6, 89], 
+                                                    [2, 4, 99], [10, 4, 63]], 
+                                                    [[9, 7, 65], [9, 6, 1], [4, 5, 34], [10, 8, 43], 
+                                                    [3, 7, 76], [4, 2, 15], [7, 6, 52], [2, 0, 50], [7, 6, 62], 
+                                                    [1, 0, 81], [4, 5, 35], [0, 11, 86], [12, 5, 50], 
+                                                    [11, 2, 2], [9, 5, 6], [12, 0, 95], [10, 6, 9], [9, 4, 73], 
+                                                    [6, 10, 48], [12, 0, 91], [9, 10, 58], [9, 8, 73], [2, 3, 44], 
+                                                    [7, 11, 83], [5, 3, 14], [6, 2, 33]]);
+Console.WriteLine(string.Join(" ", result5));
