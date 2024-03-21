d8fc1e19327f0fc58771370e6a79b4b10b8802d6
GeorgeD615 <g.davlyatshin@gmail.com>
2024-03-21 21:08:42 +0300

    remove unnecessary usings

--- Davlyatshin/Davlyatshin/2374.cs
+++ Davlyatshin/Davlyatshin/2374.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Davlyatshin
+﻿namespace Davlyatshin
 {
     public partial class Solution
     {
